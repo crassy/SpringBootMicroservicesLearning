2d54bf7ddc59d7e45b5586c181f8d9f86af324c0
Crassy <crassydeveloper@gmail.com>
2023-03-10 01:07:33 +0530

    Actuator is added.

--- address-service/src/main/java/balu/sbms/addressservice/controller/AddressController.java
+++ address-service/src/main/java/balu/sbms/addressservice/controller/AddressController.java
@@ -4,10 +4,13 @@ import balu.sbms.addressservice.request.CreateAddressRequest;
 import balu.sbms.addressservice.response.AddressResponse;
 import balu.sbms.addressservice.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/address")
+@RefreshScope
 public class AddressController {
 
     @Autowired
@@ -22,5 +25,14 @@ public class AddressController {
 
     public AddressResponse getById(@PathVariable long id){
         return addressService.getById(id);
+    }
+
+
+    @Value("${address.test}")
+    private String test;
+    @GetMapping("/test")
+    public String test(){
+
+        return test;
     }
 }
